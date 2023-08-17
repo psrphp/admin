b242db6d5643b524cd78be562a7bbd2215edd3e8
John Doe <johndoe@example.com>
2023-08-17 14:50:58 +0800

    1

--- src/template/default/common/header.php
+++ src/template/default/common/header.php
@@ -5,6 +5,36 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>{$config->get('copyright.name@psrphp.admin', '后台管理系统')}</title>
+    <style>
+        a {
+            text-decoration: none;
+        }
+
+        table {
+            border-collapse: collapse;
+        }
+
+        table th,
+        table td {
+            border: 1px solid #ddd;
+            padding: 5px;
+        }
+
+        fieldset {
+            border-width: 1px;
+            border-color: #888;
+        }
+
+        fieldset legend {
+            font-weight: bold;
+        }
+
+        select,
+        input,
+        textarea {
+            padding: 2px;
+        }
+    </style>
 </head>
 
 <body>
