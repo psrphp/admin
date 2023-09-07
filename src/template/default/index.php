113d0f4d42dc6cea82730551b4ab3b197514046c
John Doe <johndoe@example.com>
2023-09-07 17:30:15 +0800

    1

--- src/template/default/index.php
+++ src/template/default/index.php
@@ -18,6 +18,9 @@
         background: #000;
         display: flex;
         align-items: center;
+        box-shadow: 0px 1px 20px 0px;
+        z-index: 111;
+        border-bottom: 1px solid #252525;
     }
 
     .left {
@@ -27,7 +30,8 @@
         bottom: 0;
         width: 200px;
         overflow: auto;
-        background: #eee;
+        background: #000;
+        color: gray;
     }
 
     .right {
@@ -39,12 +43,17 @@
         overflow: auto;
         background: #fff;
     }
+
+    a {
+        text-decoration: none;
+        color: #202020;
+    }
 </style>
 <div class="top">
     <div style="color: #ddd;font-size: 1.5em;margin-left: 10px;" onclick="this.parentNode.nextElementSibling.nextElementSibling.style.left=this.parentNode.nextElementSibling.nextElementSibling.style.left=='200px'?'0':'200px'"><span style="margin-right:5px">≡</span>{$config->get('copyright.name@psrphp.admin', '后台管理系统')}</div>
 </div>
 <div class="left">
-    <div style="padding: 10px;">
+    <div style="margin: 15px 10px 0 10px;padding: 7px;border: 1px solid #878787;background: #827717;color: #202020;">
         <div>
             <span>欢迎您: {$account->getName($auth->getId())}</span>
             <span>
@@ -56,8 +65,16 @@
             <a href="{echo $router->build('/psrphp/admin/my/password')}" target="main">[修改密码]</a>
         </div>
     </div>
-    <hr>
-    <div style="display: flex;flex-direction: column;gap:5px;padding: 10px;">
+    <style>
+        .menu>div>a {
+            color: #aaa;
+        }
+
+        .menu>div>a:hover {
+            color: #ffffff;
+        }
+    </style>
+    <div class="menu" style="display: flex;flex-direction: column;gap:5px;padding: 10px;">
         <div>
             <a href="{echo $router->build('/psrphp/admin/widget/index')}" target="main">主页</a>
         </div>
